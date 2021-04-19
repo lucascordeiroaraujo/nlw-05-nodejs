4293d2ac81fcb0db7a5f72968e76c3d47fdad818
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-04-19 13:45:17 -0300

    :rocket: initial project structure

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Olá NLW 05',
+  })
+})
+
+app.post('/', (request, response) => {
+  return response.json({
+    message: 'Usuário salvo com sucesso',
+  })
+})
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333')
+})
